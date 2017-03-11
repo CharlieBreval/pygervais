b2d8d33a2d6196aa317281bb028989a554e63f4a
Charlie Breval <cbreval@lafourchette.com>
2017-03-11 11:46:57 +0100

    fix

--- src/AdminBundle/Form/PostType.php
+++ src/AdminBundle/Form/PostType.php
@@ -4,6 +4,7 @@ namespace AdminBundle\Form;
 
 use Ivory\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
